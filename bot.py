d9726fdb74daedfae94b52cee4cc42b1e54f7320
Anaerob <thexbxeh@gmail.com>
2020-08-06 20:23:34 +1000

    last release

--- bot.py
+++ bot.py
@@ -1,51 +1,82 @@
+"""
+Скрипт обработки сообщений посылаемых боту
+"""
 import telebot
 import requests
 
-token = '1216892494:AAENx9So0e0_Drf78BCnMRRpJLxfeYgFCug'
+token = '1216892494:AAFkqmUvzQaisYLkyoT0X-mmj8PS7M40tZU'
 
 bot = telebot.TeleBot(token)
 
+# адрес api-сервера
 url_api = 'http://127.0.0.1:5000/api/'
 url = 'http://127.0.0.1:5000/'
 
 error_emoji = u'\U0000274C'
 success_emoji = u'\U00002705'
+face_emoji = u'\U0001F31D'
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    client_id = message.from_user.id
-    print(client_id)
-    bot.send_message(message.chat.id, 'Привет, я сохраню твои аудиозаписи в формате .wav'
+    """
+    Обработчик команды старт
+    """
+    name = message.from_user.first_name
+    bot.send_message(message.chat.id, 'Привет ' + name + ', я сохраню твои'
+                                      ' аудиосообщения в формате .wav'
                                       ' c частотой дискретизации 16kHz, а также фотографии'
-                                      ' если на них будут лица ')
+                                      ' если на них будут лица ' + face_emoji + '\n/github')
+
+
+@bot.message_handler(commands=['github'])
+def github(message):
+    """
+    Обработчик команды github, отправляет ссылку на репозиторий проекта
+    """
+    bot.send_message(message.chat.id, 'https://github.com/chichur/bot-keeper')
 
 
 @bot.message_handler(content_types=['voice'])
 def get_voice_message(message):
+    """
+    Обработчик голосовых сообщений
+    """
     file_info = bot.get_file(message.voice.file_id)
     client_id = message.from_user.id
-    print(client_id)
     handler_exception(message, file_info, client_id)
 
 
 @bot.message_handler(content_types=['photo'])
 def get_photos_message(message):
+    """
+    Обработчик фотографий
+    """
     file_info = bot.get_file(message.photo[0].file_id)
     client_id = message.from_user.id
     handler_exception(message, file_info, client_id)
 
 
 def handler_exception(message, file_info, client_id):
+    """
+    Обработчик исключений возникающих при обращении
+    к апи-серверу
+    :param message: сообщение
+    :param file_info: файл принятый ботом
+    :param client_id: уникальный номер пользователя
+    :return:
+    """
     try:
+        # обращение к апи серверу с помощью метода GET
         req_api = requests.get(url_api + '{0}/{1}'.format(file_info.file_path, client_id))
     except requests.ConnectionError:
+        # обрабатываем исключения ConnectionError, как наиболее частое
         bot.send_message(message.chat.id, error_emoji + ' Ошибка подключения к серверу данных')
     except Exception as e:
         bot.send_message(message.chat.id, 'Неизвестная ошибка: {0}'.format(e))
     else:
         if message.voice:
-            bot.send_message(message.chat.id, success_emoji + ' Ваша аудиозапись конвертирована\n'
+            bot.send_message(message.chat.id, success_emoji + ' Ваше аудиосообщение конвертировано\n'
                                                               'Скачать: ' + url + req_api.text)
         elif message.photo:
             if req_api.text == 'error':
--- keeper.py
+++ keeper.py
@@ -9,26 +9,37 @@ from detect_faces import Detector
 
 app = Flask(__name__)
 
+# инициализируем экземляр класса детектора
+# чтобы не загружать повторно тренеровочные данные
+# (haarcascade_frontalface_default.xml)
 d = Detector()
 
 
 @app.route('/api/voice/<file_path>/<uid>', methods=['GET'])
 def api_voice(file_path, uid):
     try:
+        # получаем файл с апи-сервера телеграм ботов
         file = requests.get('https://api.telegram.org/file/bot{0}/voice/{1}'.format(token, file_path))
     except:
         return 'error'
     else:
+        # создаем дерикторию <uid> если ее нету
         Path('static/voices/' + uid).mkdir(parents=True, exist_ok=True)
+        # путь входящего файла *.oga
         path = os.path.join('static', 'voices', uid, file_path)
 
+        # сохраняем файл *.oga
         with open(path, 'wb') as f:
             f.write(file.content)
 
+        # путь исходящего файла *.wav
         out_path = os.path.join('static', 'voices', uid, 'audio_message_' + str(counter(uid)))
+        # конвертируем в *.wav
         decode_audio(path, out_path)
+        # удаляем ненужный файл *.oga
         os.remove(path)
         out_path = out_path + '.wav'
+        # возращаем путь url нашего нового файла
         return out_path.replace('\\', '/')
 
 
@@ -42,6 +53,7 @@ def api_photo(file_path, uid):
         Path('static/photos/' + uid).mkdir(parents=True, exist_ok=True)
         path = os.path.join('static', 'photos', uid, file_path)
 
+        # если на фото обнаружено лицо сохраняем
         if d.detect_face(file.content):
             with open(path, 'wb') as f:
                 f.write(file.content)
@@ -51,14 +63,26 @@ def api_photo(file_path, uid):
 
 
 def decode_audio(in_filename, o_filename):
+    """
+    Функция конвертации oga в wav.
+    Использован биндинг ffmpeg-python программы ffmpeg
+    :param in_filename: входящий файл
+    :param o_filename: исходящий файл
+    """
     input = ffmpeg.input(in_filename)
     out = ffmpeg.output(input, o_filename + '.wav', format='wav', acodec='pcm_s16le', ac=1, ar='16000') \
         .overwrite_output()\
         .run()
-    return out
 
 
 def counter(uid):
+    """
+    Функция счетчик аудиосообщений пользователей.
+    Формирует json файл, формата ключ - пользотель,
+    значение - номер текущей записи.
+    :param uid:
+    :return: возращает текущий номер записи пользователя
+    """
     with open('counters.json', 'r+') as f:
         pairs = json.loads(f.read())
         if uid in pairs:
